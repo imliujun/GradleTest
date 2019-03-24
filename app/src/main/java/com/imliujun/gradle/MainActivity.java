13946f17008dd40a4504f58dfa120cb7c93ae0a2
LiuJun <mail@liujun.info>
2019-03-24 18:58:51 +0800

    增加自动加固和自动上传蒲公英命令

--- app/src/main/java/com/imliujun/gradle/MainActivity.java
+++ app/src/main/java/com/imliujun/gradle/MainActivity.java
@@ -1,16 +1,36 @@
 package com.imliujun.gradle;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //可以去 BuildConfig 中查看Gradle中定义的常量
         Log.i("tag", BuildConfig.DOMAIN_NAME);
+        //获取渠道名
+        String channel = getMetaDataInApp("UMENG_CHANNEL");
+        Log.i("channel", channel);
+    }
+    
+    public String getMetaDataInApp(@NonNull final String key) {
+        String value = "";
+        PackageManager pm = getPackageManager();
+        String packageName = getPackageName();
+        try {
+            ApplicationInfo ai = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+            value = String.valueOf(ai.metaData.get(key));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return value;
     }
 }
